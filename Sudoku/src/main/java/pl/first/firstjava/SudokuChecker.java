a6a9f54cb5c864607240a1934c419032be662ac2
242548 Szymczak Jakub <242548@edu.p.lodz.pl>
2022-11-16 20:04:49 +0100

    poprawiony SudokuChecker

--- Sudoku/src/main/java/pl/first/firstjava/SudokuChecker.java
+++ Sudoku/src/main/java/pl/first/firstjava/SudokuChecker.java
@@ -1,21 +1,30 @@
 package pl.first.firstjava;
-import java.util.*;
+
+//import java.util;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public abstract class SudokuChecker {
-    protected SudokuField[] field2;
-    protected List<SudokuField> field = new ArrayList<SudokuField>(Arrays.asList(new SudokuField[9]));
+    //protected SudokuField[] field2;
+    protected final int size = 9;
+    protected List<SudokuField> field = new ArrayList<>();
+
     public SudokuChecker(SudokuField[] fields) {
+        //for(int i=0;i<size;i++){
+        //    this.field.add(i,fields[i]);
+        //}
         this.field.addAll(Arrays.asList(fields));
     }
 
     public boolean verify() {
         boolean[] numbersCheck = new boolean[9];
-        for (int i = 0; i < field.size(); i++) {
-            if (field.get(i).getFieldValue() != 0) {
-                if (numbersCheck[field.get(i).getFieldValue() - 1]) {
+        for (SudokuField sudokuField : field) {
+            if (sudokuField.getFieldValue() != 0) {
+                if (numbersCheck[sudokuField.getFieldValue() - 1]) {
                     return false;
                 }
-                numbersCheck[field.get(i).getFieldValue() - 1] = true;
+                numbersCheck[sudokuField.getFieldValue() - 1] = true;
             }
         }
         return true;
