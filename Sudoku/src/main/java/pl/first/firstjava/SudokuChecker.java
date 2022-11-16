4574adbce119067b542c7ee82ae25264b1c6d91c
242548 Szymczak Jakub <242548@edu.p.lodz.pl>
2022-11-16 14:06:03 +0100

    zamiana tablicy na liste w SudokuChecker, dodanie licencji

--- Sudoku/src/main/java/pl/first/firstjava/SudokuChecker.java
+++ Sudoku/src/main/java/pl/first/firstjava/SudokuChecker.java
@@ -1,20 +1,23 @@
 package pl.first.firstjava;
+import java.util.*;
 
 public abstract class SudokuChecker {
-    protected SudokuField[] field;
-
+    protected SudokuField[] field2;
+    protected List<SudokuField> field = Arrays.asList(new SudokuField[9]);
     public SudokuChecker(SudokuField[] fields) {
-        this.field = fields;
+        for(int i=0;i<fields.length;i++){
+            this.field.add(fields[i]);
+        }
     }
 
     public boolean verify() {
         boolean[] numbersCheck = new boolean[9];
-        for (int i = 0; i < field.length; i++) {
-            if (field[i].getFieldValue() != 0) {
-                if (numbersCheck[field[i].getFieldValue() - 1]) {
+        for (int i = 0; i < field.size(); i++) {
+            if (field.get(i).getFieldValue() != 0) {
+                if (numbersCheck[field.get(i).getFieldValue() - 1]) {
                     return false;
                 }
-                numbersCheck[field[i].getFieldValue() - 1] = true;
+                numbersCheck[field.get(i).getFieldValue() - 1] = true;
             }
         }
         return true;
