7ec5c4b309b4228af6923d58988ec9f04210bb32
242510 Krystian Romkowski <242510@edu.p.lodz.pl>
2022-11-20 17:56:13 +0100

    Utworzenie metdo equals, toString, hashCode w SudokuField

--- Sudoku/src/main/java/pl/first/firstjava/SudokuField.java
+++ Sudoku/src/main/java/pl/first/firstjava/SudokuField.java
@@ -2,6 +2,10 @@ package pl.first.firstjava;
 
 import java.util.HashSet;
 import java.util.Set;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 
 public class SudokuField {
 
@@ -29,5 +33,23 @@ public class SudokuField {
         checkers.add(checker);
     }
 
+    @Override
+    public boolean equals(Object obj){
+        if (obj == null) { return false; }
+        if (obj == this) { return true; }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        return new EqualsBuilder().append(this.value,((SudokuField)obj).value).isEquals();
+    }
 
+    @Override
+    public int hashCode(){
+        return new HashCodeBuilder(17, 37).append(this.value).toHashCode();
+    }
+
+    @Override
+    public String toString(){
+        return new ToStringBuilder(this).append("value", value).toString();
+    }
 }
