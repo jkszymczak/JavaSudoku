5dfdeefe9ca5b68ea9caa26ca7b1ab1e5e25098d
Krystian Romkowski <242510@edu.p.lodz.pl>
2023-01-12 00:26:33 +0100

    zapis i odczyt dodany

--- Sudoku/View/src/main/java/pl/first/firstjava/view/BoardController.java
+++ Sudoku/View/src/main/java/pl/first/firstjava/view/BoardController.java
@@ -2,7 +2,15 @@ package pl.first.firstjava.view;
 
 //import static pl.first.firstjava.DifficultyLevel.Easy;
 
+import javafx.beans.binding.Bindings;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+import java.io.File;
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -14,25 +22,26 @@ import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.util.converter.NumberStringConverter;
 import pl.first.firstjava.BacktrackingSudokuSolver;
 import pl.first.firstjava.DifficultyLevel;
+import pl.first.firstjava.FileSudokuBoardDao;
 import pl.first.firstjava.SudokuBoard;
 
 
-/* Controller needs to implement Initializable as JavaFX relies on the class having
- * an "initialize" method. It is going to execute the "initialize" method only AFTER the layout
- * file has been loaded.
- */
+
 public class BoardController implements Initializable {
     int playerselectedcol;
     int playerselectedrow;
 
     private SudokuBoard sudokuBoard;
+    private SudokuBoard sudokuBoardSolved;
 
-    @FXML // The FXML loader is going to inject from the layout
+
+    @FXML
     Button buttonOne; // remember our
-    // fx:id's for our buttons? name should be the same in order for the FXMLLoader
-    // to find it.
+
     @FXML
     Button buttonTwo;
     @FXML
@@ -56,13 +65,64 @@ public class BoardController implements Initializable {
     @FXML
     Button buttonHard;
     @FXML
+    Button buttonPL;
+    @FXML
+    Button buttonEN;
+    @FXML
     Canvas canvas;
+    @FXML
+    Pane pane;
 
     public void difficulty(int lvl) throws CloneNotSupportedException {
         sudokuBoard = (SudokuBoard) generowanie().clone();
-
         DifficultyLevel probne = DifficultyLevel.Easy;
         probne.zeroFields(lvl, sudokuBoard);
+    }
+    private static String randomString(int n)
+    {
+
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+
+
+            int index
+                    = (int)(AlphaNumericString.length()
+                    * Math.random());
+
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+
+        return sb.toString();
+    }
+    /*@FXML
+    public void polski(ActionEvent event) {
+        Locale.setDefault(new Locale("pl"));
+    }*/
+    @FXML
+    public void save() {
+        FileSudokuBoardDao zapisywacz = new FileSudokuBoardDao(randomString(5));
+        zapisywacz.write(sudokuBoard);
+    }
+    @FXML
+    public void read() throws CloneNotSupportedException {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Open File");
+        String fileName = chooser.showOpenDialog(new Stage()).getName().replaceFirst("[.][^.]+$", "");
+        System.out.println(fileName);
+        FileSudokuBoardDao plik = new FileSudokuBoardDao(fileName);
+        sudokuBoard = plik.read();
+        GraphicsContext context = canvas.getGraphicsContext2D();
+        drawOnCanvas(context);
+
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        sudokuBoardSolved = (SudokuBoard) sudokuBoard.clone();
+        sudokuBoardSolved.solveGame(solver);
     }
 
     @FXML
@@ -89,9 +149,7 @@ public class BoardController implements Initializable {
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         generowaniePustej();
-        // Get graphics context from canvas
         GraphicsContext context = canvas.getGraphicsContext2D();
-        // Call drawOnCanvas method, with the context we have gotten from the canvas
 
         try {
             drawOnCanvas(context);
@@ -107,73 +165,66 @@ public class BoardController implements Initializable {
         return sudokuBoard1;
     }
 
+    public void canvasMouseClicked() {
+        canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                int mouse_x = (int) event.getX();
+                int mouse_y = (int) event.getY();
+
+
+                playerselectedrow = (int) (mouse_y / 50);
+                playerselectedcol = (int) (mouse_x / 50);
+
+                //get the canvas graphics context and redraw
+                try {
+                    drawOnCanvas(canvas.getGraphicsContext2D());
+                } catch (CloneNotSupportedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
     public void generowaniePustej() {
         sudokuBoard = new SudokuBoard();
     }
 
     public void drawOnCanvas(GraphicsContext context) throws CloneNotSupportedException {
-        /*
-         * button_easy.setOnAction(event -> {
-         * try {
-         * difficulty(1);
-         * } catch (CloneNotSupportedException e) {
-         * throw new RuntimeException(e);
-         * }
-         * });
-         * button_medium.setOnAction(event -> {
-         * try {
-         * difficulty(2);
-         * } catch (CloneNotSupportedException e) {
-         * throw new RuntimeException(e);
-         * }
-         * });
-         * button_medium.setOnAction(event -> {
-         * try {
-         * difficulty(3);
-         * } catch (CloneNotSupportedException e) {
-         * throw new RuntimeException(e);
-         * }
-         * });
-         */
         context.clearRect(0, 0, 450, 450);
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
-                // finds the y position of the cell, by
-                // multiplying the row number by 50, which is the height of a row
-                // then adds 2, to add some offset
+
                 int positionY = row * 50 + 2;
 
-                // finds the x position of the cell, by multiplying the column
-                // number by 50, which is the width of a
-                // then add 2, to add some offset
+
                 int positionX = col * 50 + 2;
 
-                // defines the width of the square as 46 instead of 50, to account for the
-                // 4px total of blank space
-                // as we are drawing squares, the height is going to be the same
+
                 int width = 46;
 
-                // set the fill color to white (you could set it to whatever you want)
                 context.setFill(Color.WHITE);
 
-                // draw a rounded rectangle with the calculated position and width. The last two
-                // arguments specify the
-                // rounded corner arcs width and height.
-                // Play around with those if you want.
+
                 context.fillRoundRect(positionX, positionY, width, width, 10, 10);
             }
         }
-        // draw highlight around selected cell
-        // set stroke color to res
+
         context.setStroke(Color.BLUE);
-        // set stroke width to 5px
         context.setLineWidth(5);
-        // draw a strokeRoundRect using the same technique we used for drawing our
-        // board.
+
         context.strokeRoundRect(playerselectedcol * 50 + 2,
                 playerselectedrow * 50 + 2, 46, 46, 10, 10);
+        /*for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                sudokuBoard.get(row,col).addListener((observable, oldValue, newValue) -> {
+                    context.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                    context.fillText(newValue, row, col);
+                });
+            }
+        }*/
 
-        // draw the initial numbers from our GameBoard instance
+
         int[][] initial = new int[9][9];
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
@@ -185,72 +236,23 @@ public class BoardController implements Initializable {
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
 
-                // finds the y position of the cell, by multiplying the row number by 50, which
-                // is the height of a row in pixels then adds 2, to add some offset
+
                 int positionY = row * 50 + 30;
 
-                // finds the x position of the cell, by multiplying the column number by 50,
-                // which is the width of a column in pixels then add 2, to add some offset
+
                 int positionX = col * 50 + 20;
 
-                // set the fill color to white (you could set it to whatever you want)
                 context.setFill(Color.BLACK);
 
-                // set the font, from a new font, constructed from the system one, with size 20
                 context.setFont(new Font(20));
 
-                // check if value of coressponding initial array position is not 0, remember
-                // that
-                // we treat zeroes as squares with no values.
+
                 if (initial[row][col] != 0) {
 
-                    // draw the number using the fillText method
                     context.fillText(initial[row][col] + "", positionX, positionY);
+
                 }
             }
         }
     }
-    /*
-     * void probna(GraphicsContext context) {
-     * int[][] initial= new int [9][9];
-     * for(int row = 0; row<9; row++) {
-     * for (int col = 0; col < 9; col++) {
-     * initial[row][col] = sudokuBoard.get(row,col);
-     * }
-     * }
-     * 
-     * 
-     * 
-     * // for loop is the same as before
-     * for(int row = 0; row<9; row++) {
-     * for(int col = 0; col<9; col++) {
-     * 
-     * // finds the y position of the cell, by multiplying the row number by 50,
-     * which
-     * // is the height of a row in pixels then adds 2, to add some offset
-     * int position_y = row * 50 + 30;
-     * 
-     * // finds the x position of the cell, by multiplying the column number by 50,
-     * // which is the width of a column in pixels then add 2, to add some offset
-     * int position_x = col * 50 + 20;
-     * 
-     * // set the fill color to white (you could set it to whatever you want)
-     * context.setFill(Color.BLACK);
-     * 
-     * // set the font, from a new font, constructed from the system one, with size
-     * 20
-     * context.setFont(new Font(20));
-     * 
-     * // check if value of coressponding initial array position is not 0, remember
-     * that
-     * // we treat zeroes as squares with no values.
-     * if(initial[row][col]!=0) {
-     * 
-     * // draw the number using the fillText method
-     * context.fillText(initial[row][col] + "", position_x, position_y);
-     * }
-     * }
-     * }
-     * }
-     */
 }
